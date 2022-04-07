2eebda7042a6e8b4154fc32f362085382618bbed
Naufal Ibrahim <naufal.ti@outlook.com>
2022-04-07 15:33:54 +0700

    Create POM Farmer Page

---
+++ src/main/java/page/FarmerPage.java
@@ -0,0 +1,96 @@
+package page;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FarmerPage {
+    WebDriver driver;
+
+    //Define locators in Barn Dashboard
+
+    By dasboard_button_loc= By.xpath("//button[normalize-space()='Dashboard']");
+    By distribution_button_loc= By.xpath("//button[normalize-space()='Distribution']");
+
+    //get Barn Name by Index
+    By barnName_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[2]");
+    }
+    //Get Barn Start Periode by Index
+    By barnStartPeriode_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[3]");
+    }
+    //Get Barn End Periode by Index
+    By barnEndPeriode_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[4]");
+    }
+    //Get Barn Carrot Amount by Index
+    By barnCarrotAmount_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[5]");
+    }
+    //Get Barn Distributed Carrot by Index
+    By barnDistributedCarrot_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[6]");
+    }
+    //Get Barn Status by Index
+    By barnStatus_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[7]");
+    }
+    //Get Manage Button by Index
+    public By manage_button_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[8]/button[1]");
+    }
+    //Get History Button by Index
+    public By history_button_loc(int index){
+        return By.xpath("//tbody["+index+"]/tr["+index+"]/td[8]/button[2]");
+    }
+    //Get Create Barn Button
+    public By createBarn_button_loc= By.xpath("//button[normalize-space()='Create Barn']");
+
+    public FarmerPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    //Define Methods that are commonly used
+    public void assertInFarmerPage(){
+        //Assert URL is as expected
+        Assert.assertEquals(this.driver.getCurrentUrl(),"http://localhost:3000/farmer");
+    }
+    public void clickDashboardButton(){
+        this.driver.findElement(dasboard_button_loc).click();
+    }
+    public void clickDistributionButton(){
+        this.driver.findElement(distribution_button_loc).click();
+    }
+    public void clickManageButton(int index){
+        this.driver.findElement(manage_button_loc(index)).click();
+    }
+    public void clickHistoryButton(int index){
+        this.driver.findElement(history_button_loc(index)).click();
+    }
+    public String getBarnName(int index){
+        return this.driver.findElement(barnName_loc(index)).getText();
+    }
+    public String getBarnStartPeriode(int index){
+        return this.driver.findElement(barnStartPeriode_loc(index)).getText();
+    }
+    public String getBarnEndPeriode(int index){
+        return this.driver.findElement(barnEndPeriode_loc(index)).getText();
+    }
+    public String getBarnCarrotAmount(int index){
+        return this.driver.findElement(barnCarrotAmount_loc(index)).getText();
+    }
+    public String getBarnDistributedCarrot(int index){
+        return this.driver.findElement(barnDistributedCarrot_loc(index)).getText();
+    }
+    public String getBarnStatus(int index){
+        return this.driver.findElement(barnStatus_loc(index)).getText();
+    }
+
+    public void clickCreateBarnButton(){
+        this.driver.findElement(
+            createBarn_button_loc).click();
+    }
+
+    
+}
